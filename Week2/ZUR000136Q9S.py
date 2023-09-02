b88d524e826de7cdec2cf2e608555ce13ba5f7e7
rhedwan <adesolaridwan2003@gmail.com>
2023-09-02 14:56:22 +0100

    implement cascading palindrome

---
+++ Week2/ZUR000136Q9S.py
@@ -0,0 +1,53 @@
+class CascadingPalindrome:
+  def __init__(self):
+    # the varible to be used to store the results
+    self.parameter = []
+
+  #  check if the parameter is a palindrome
+  def isPalindrome(self, word):
+    return word == word[::-1]
+
+  #  check if the parameter is satisfy the cascading conditions
+  def checkCascading(self, words):
+    try:
+    # splitting the words into a list of words
+      for word in words.split():
+
+        # check if the word in the list is a palindrome
+        if self.isPalindrome(word):
+            self.parameter.append(word)
+
+        result = " ".join(self.parameter)
+        return result if result else "This does not have a palindrome in it."
+
+    except AttributeError:
+      # print("Only a parameter of the string type is supported")
+      return "Only a parameter of the string type is supported."
+
+
+word1 = CascadingPalindrome().checkCascading("1230321")
+word2 = CascadingPalindrome().checkCascading("1230321 09234 0124210")
+word3 = CascadingPalindrome().checkCascading("abcd5dcba 1230321 09234 0124210")
+
+# this ensures that the function returns the correct output when the parameter is not a string
+word4 = CascadingPalindrome().checkCascading({})
+
+# this ensures that the function returns the correct output when the parameter is not a string
+word5 = CascadingPalindrome().checkCascading([])
+
+word6 = CascadingPalindrome().checkCascading("racecar")
+word7 = CascadingPalindrome().checkCascading("madam")
+word8 = CascadingPalindrome().checkCascading(1)
+word9 = CascadingPalindrome().checkCascading("king")
+word10 = CascadingPalindrome().checkCascading("01.121")
+
+print(word1)
+print(word2)
+print(word3)
+print(word4)
+print(word5)
+print(word6)
+print(word7)
+print(word8)
+print(word9)
+print(word10)
